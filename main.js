cd8cb001fadc91e1b2c46b9c9cf7d83b2e233646
Diogo <di_smgomes@hotmail.com>
2023-09-19 13:32:57 -0300

    finished the first version of the calculator using the 'eval' function to calculate math values

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const display = document.querySelector('.display')
-const btnClear = document.querySelector('.btn-clear')
-const btnDelete = document.querySelector('.btn-delete')
+const display = document.querySelector('.display');
+const btnClear = document.querySelector('.btn-clear');
+const btnDelete = document.querySelector('.btn-delete');
 
 const click = document.addEventListener('click', (e) => {
-    const eventClick = e.target
+    const eventClick = e.target;
     if (eventClick.classList.contains('btn-num')) {
         console.log(eventClick.innerText);
         display.value += eventClick.innerText;
@@ -11,6 +11,20 @@ const click = document.addEventListener('click', (e) => {
         display.value = display.value.slice(0, -1);
     } else if (eventClick.classList.contains('btn-clear')) {
         display.value = '';
+    } else if (eventClick.classList.contains('btn-eq')) {
+        let equals = display.value
+        try {
+            equals = eval(equals)
+            console.log(equals)
+            if (!equals) {
+                alert("Invalid math");
+                return;
+            }
+            display.value = equals;
+        } catch(e) {
+            alert("Invalid math");
+            return;
+        };
     };
-})
+});
 
